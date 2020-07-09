931830dce163d8a34786cc04c31f62446a97162a
Pratik Nilange <51482651+PratikNilange@users.noreply.github.com>
2020-07-09 23:24:07 +0530

    Add files via upload

--- db/2020/music.js
+++ db/2020/music.js
@@ -4,32 +4,64 @@ var $table = $('#table');
     var mydata = 
 [
     {
-        "id": 2,
-        "name": "test2",
-        "price": "$2"
+        "star": "<a rel=\"tooltip\" title=\"Gold Star\" data-placement = \"left\"><img src=\"certification/music/gold-01.png\" width=\"40px;\" alt=\"Gold Star\"></a>",
+        "artist": "Tones And I",
+        "title": "Dance Monkey",
+        "date": "11 July, 2020"
     },
     {
-        "id": 3,
-        "name": "test3",
-        "price": "$3"
+        "star": "<a rel=\"tooltip\" title=\"Gold Star\" data-placement = \"left\"><img src=\"certification/music/gold-01.png\" width=\"40px;\" alt=\"Gold Star\"></a>",
+        "artist": "Powfu",
+        "title": "death bed (coffee for your head) ft. beabadoobee",
+        "date": "10 July, 2020"
+    },
+    {
+        "star": "<a rel=\"tooltip\" title=\"Gold Star\" data-placement = \"left\"><img src=\"certification/music/gold-01.png\" width=\"40px;\" alt=\"Gold Star\"></a>",
+        "artist": "Harry Styles",
+        "title": "Watermelon Sugar",
+        "date": "10 July, 2020"
+    },
+    {
+        "star": "<a rel=\"tooltip\" title=\"Gold Star\" data-placement = \"left\"><img src=\"certification/music/gold-01.png\" width=\"40px;\" alt=\"Gold Star\"></a>",
+        "artist": "Saint Jhn",
+        "title": "Roses - Imanbek Remix",
+        "date": "10 July, 2020"
+    },
+    {
+        "star": "<a rel=\"tooltip\" title=\"Gold Star\" data-placement = \"left\"><img src=\"certification/music/gold-01.png\" width=\"40px;\" alt=\"Gold Star\"></a>",
+        "artist": "DaBaby, Roddy Ricch",
+        "title": "Rockstar",
+        "date": "10 July, 2020"
     },
     {
         "star": "<a rel=\"tooltip\" title=\"Platinum Star\" data-placement = \"left\"><img src=\"certification/music/platinum-01.png\" width=\"40px;\" alt=\"Platinum Star\"></a>",
         "artist": "The Weeknd",
         "title": "Blinding Lights",
-        "date": "10 July, 2020"
+        "date": "09 July, 2020"
     },
     {
         "star": "<a rel=\"tooltip\" title=\"Gold Star\" data-placement = \"left\"><img src=\"certification/music/gold-01.png\" width=\"40px;\" alt=\"Gold Star\"></a>",
         "artist": "The Weeknd",
         "title": "Blinding Lights",
-        "date": "06 June, 2020"
+        "date": "06 June, 2020*"
     },
     {
         "star": "<a rel=\"tooltip\" title=\"Gold Star\" data-placement = \"left\"><img src=\"certification/music/gold-01.png\" width=\"40px;\" alt=\"Gold Star\"></a>",
         "artist": "Maroon 5",
         "title": "Memories",
         "date": "06 June, 2020"
+    },
+    {
+        "star": "<a rel=\"tooltip\" title=\"Platinum Star\" data-placement = \"left\"><img src=\"certification/music/platinum-01.png\" width=\"40px;\" alt=\"Platinum Star\"></a>",
+        "artist": "Maroon 5",
+        "title": "Girls Like You (feat. Cardi B)",
+        "date": "06 June, 2020"
+    },
+    {
+        "star": "<a rel=\"tooltip\" title=\"Gold Star\" data-placement = \"left\"><img src=\"certification/music/gold-01.png\" width=\"40px;\" alt=\"Gold Star\"></a>",
+        "artist": "Jon Pardi",
+        "title": "Heatache On The Dance Floor",
+        "date": "03 June, 2020"
     }
 ];
 
